e9e49332d71148cd1eb54208965bb350a67db4d6
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-07-29 06:31:47 -0300

    começo do WineSpirits

--- winespirits/src/app/app.module.ts
+++ winespirits/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { AppRoutingModule } from './routings/AppRoutingModule ';
+import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FrontPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ winespirits/src/app/services/login-service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+  private baseUrl = 'http://localhost:3000';
+  constructor(private http: HttpClient) { }
+  verificarCadastro(email: string, password: string): Observable<any> {
+    const url = `${this.baseUrl}/formData`;
+    return this.http.get(url, { params: { email, password } });
+  }
+}
