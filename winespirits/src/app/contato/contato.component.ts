423e5832f32230be3842362468e9a91843c3755b
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-14 08:31:08 -0300

    feito o service e o back-end

--- winespirits/src/app/contato/contato.component.ts
+++ winespirits/src/app/contato/contato.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { ContactService } from '../services/contact.service';
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
-export class ContatoComponent implements OnInit {
-  contactForm: FormGroup = this.formBuilder.group({
-    subjectTitle: ['', Validators.required],
-    userEmail: ['', [Validators.required, Validators.email]],
-    message: ['', Validators.required]
-  });
+export class ContatoComponent {
+  contactForm!: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private contactService: ContactService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      subjectTitle: ['', Validators.required],
+      userEmail: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
 
   onSubmit(): void {
     if (this.contactForm.valid) {
-      this.submitted = true;
+      this.contactService.sendContactForm(this.contactForm.value).subscribe(() => {
+        this.submitted = true;
+      });
     }
   }
 }
