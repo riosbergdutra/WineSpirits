00597818c1647314988bcfb51b9f20253e228bc8
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-17 09:29:55 -0300

    componente de criação de vinho

---
+++ winespirits/src/app/vendedor/vendedor.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Wine } from '../interface/wine';
+@Component({
+  selector: 'app-vendedor',
+  templateUrl: './vendedor.component.html',
+  styleUrls: ['./vendedor.component.css']
+})
+export class VendedorComponent {
+  product: Wine = {
+    id: 0, // O id será gerado automaticamente pelo json-server
+    name: '',
+    year: 0,
+    type: '',
+    imageUrl: '',
+    price: 0,
+    description: '',
+    productdescription: ''
+  };
+
+  constructor(private productService: ProductService) {}
+
+  onSubmit() {
+    this.productService.createProduct(this.product).subscribe(
+      response => {
+        console.log('Produto cadastrado com sucesso:', response);
+        // Limpar o formulário após o cadastro bem-sucedido
+        this.product = {
+          id: 0,
+          name: '',
+          year: 0,
+          type: '',
+          imageUrl: '',
+          price: 0,
+          description: '',
+          productdescription: ''
+        };
+      },
+      error => {
+        console.error('Erro ao cadastrar produto:', error);
+      }
+    );
+  }
+}
